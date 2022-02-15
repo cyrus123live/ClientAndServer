4c23ce1323381e725e6a9397edfd7faf95951374
Cyrus Parsons <cyrusparsons@w134-87-138-040.wireless.uvic.ca>
2022-02-15 12:42:18 -0800

    Adding Client.py and Server.py

---
+++ server.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3           # This is server.py file
+
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345                # Reserve a port for your service.
+s.bind((host, port))        # Bind to the port
+
+s.listen(5)                 # Now wait for client connection.
+while True:
+   c, addr = s.accept()     # Establish connection with client.
+   print("Please enter a message: ", end="")
+   message = input()
+   print('Got connection from ' + str(addr[1]))
+   c.send(message.encode())
+   c.close()                # Close the connection
