67e503dc419223f14df40b36eef4a8de1f47a24f
cyrus123live <64929788+cyrus123live@users.noreply.github.com>
2022-02-15 20:15:36 -0800

    Update server.py

--- server.py
+++ server.py
@@ -8,8 +8,11 @@ port = 12345                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
 
 s.listen(5)                 # Now wait for client connection.
-while True:
+while True: # <-- this isn't boilerplate
    c, addr = s.accept()     # Establish connection with client.
+   
+   # Above this is boilerplate ---------
+   
    print("Please enter a message: ", end="")
    message = input()
    print('Got connection from ' + str(addr[1]))
